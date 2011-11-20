ea3c94897ac88f7ed9bea9d1fbc200083a543b50
Craig Tanis <craig-tanis@utc.edu>
2011-11-20 13:34:34 -0500

    first commit

---
+++ elisp/site-lisp/ctanis/functions.el
@@ -0,0 +1,517 @@
+;; BUFFER MANAGEMENT
+
+(defun better-display-buffer(arg)
+  "a better display buffer"
+  (interactive "B")
+  (let ((b (current-buffer)))
+    (switch-to-buffer-other-window arg)
+    (switch-to-buffer-other-window b)))
+
+
+(defun find-and-display-file (FILENAME)
+  "this finds file and displays it in other window without selecting it"
+  (interactive "FFind and display file: ")
+  (let ((buf (find-file-noselect FILENAME)))
+    (better-display-buffer buf)))
+
+
+(defun list-and-display-directory (dirname &optional switches)
+  "this finds dired listing and displays it in other window without selecting it"
+  (interactive (progn (require 'dired)	;; why doesn't autoload work?
+		      (dired-read-dir-and-switches "")))
+  (better-display-buffer (dired-noselect dirname switches)))
+
+
+(defun sink-buffer()
+"bury the current buffer, and delete the window!"
+  (interactive)
+  (let ((foo (buffer-name)))
+    (bury-buffer)
+    (message (string-append "Buried " foo))
+    (if (not (= (count-windows) 1))
+	(delete-window))))
+
+
+(defun kill-current-buffer ()
+  (interactive)
+  (if (buffer-modified-p)
+      (error "Can't kill current buffer, buffer modified.")
+    (kill-buffer (current-buffer))))
+
+
+(defun kill-other-buffer ()
+  "kill a buffer other than the current one, and stay in current buffer"
+  (interactive)
+  (save-excursion
+    (set-buffer (window-buffer (next-window)))
+    (let* ((lst (mapcar (lambda (a)
+			  (cons (buffer-name a) a))
+			(buffer-list))))
+      (kill-buffer
+       (let* ((ded-buf (completing-read 
+			(string-append
+			 "Kill (" (buffer-name) "):")
+			lst nil t nil))
+	      (real-ded (if (equal "" ded-buf)
+			    (buffer-name)
+			  ded-buf)))
+	 real-ded)))))
+
+
+
+(defun switch-windows-with-buffer (to)
+  "switches current buffer position with 'to'.  this doesn't work right if
+more than 2 windows are currently displayed."
+  (let ((buf (current-buffer)))
+    (switch-to-buffer to)
+    (switch-to-buffer-other-window buf)))
+
+
+
+(defun flip-windows ()
+  "flip positions of this window and the next-window"
+  (interactive)
+  "flip the current buffer and another's placement in the window"
+  (let* ((nextw (next-window))
+	 (tot (count-windows)))
+    (cond
+     ((< tot 2) (error "%s" "Not enough windows to flip"))
+     ((= tot 2) (funcall 'switch-windows-with-buffer (window-buffer (next-window))))
+     (t (switch-windows-with-buffer
+	 (let* ((buf (window-buffer nextw))
+		(name (buffer-name buf))
+		(ded-buf
+		 (completing-read 
+		  (string-append "Flip with (" name "): ")
+		  (mapcar (lambda (a)
+			 (cons (buffer-name a) a))
+		       (buffer-list))
+		  nil t nil)))
+	   (if (equal "" ded-buf)
+	       name
+	     ded-buf)))))))
+
+
+(defun alternate-buffer ()
+  (interactive)
+  (switch-to-buffer (other-buffer)))
+
+(defun alternate-buffer-in-other-window ()
+  (interactive)
+  (better-display-buffer (other-buffer)))
+
+
+;--------------------------------------------------
+; This is for jumping easily to commonly used buffers.
+
+
+(defvar common-buffers '(("l" . "*shell*")
+			 ("i" . "*info*")
+			 ("s" . "*scratch*")
+			 ("b" . "*Backtrace*"))
+  "This is the alist of codes to buffer names for use in switch-to-common-buffer")
+
+
+(defun switch-to-common-buffer (buf-id)
+  "Jump-to-existing-buffer with name corresponding to buf-id in
+common-buffers alist"
+  (interactive "cSwitch to indexed buffer (lisb): ")
+  (let* ((entry (assoc (char-to-string buf-id)
+		       (filter '(lambda (i)
+				  (get-buffer (cdr i)))
+			       common-buffers)))
+	 (bufname (and entry (cdr entry))))
+    (if bufname
+	(jump-to-existing-buffer bufname)
+      (error "Selection not available"))))
+
+
+(defun jump-to-existing-buffer (bufname)
+  "Jump to buffer BUFNAME.  If visible go there.  Otherwise make it visible
+and go there."
+  (let ((buf (get-buffer bufname)))
+    (if buf
+	(cond
+	 ((equal buf (current-buffer)) t)
+	 ((equal (count-windows) 1)
+	  (switch-to-buffer-other-window buf))
+	 ((get-buffer-window buf)
+	  (select-window (get-buffer-window buf)))
+	 (t (switch-to-buffer bufname))))))
+
+
+(require 'advice)
+;-----------------------------------------------------------------------------
+;i stole this stuff from eric beuscher.  i'm sick of creating buffers on
+;accident, and then having to kill them!
+
+(defadvice switch-to-buffer 
+  (before existing-buffers-only activate)
+  "When called interactively switch to existing buffers only, unless 
+when called with a prefix argument."
+  (interactive 
+   (list (read-buffer "Switch to buffer: " (other-buffer) 
+                      (null current-prefix-arg)))))
+
+(defadvice switch-to-buffer-other-window 
+  (before existing-buffers-only activate)
+  "When called interactively switch to existing buffers only, unless 
+when called with a prefix argument."
+  (interactive 
+   (list (read-buffer "Switch to buffer other window: " (other-buffer) 
+                      (null current-prefix-arg)))))
+
+(defadvice better-display-buffer 
+  (before existing-buffers-only activate)
+  "When called interactively display existing buffers only, unless 
+when called with a prefix argument."
+  (interactive 
+   (list (read-buffer "Display buffer: " (other-buffer) 
+                      (null current-prefix-arg)))))
+
+(defadvice display-buffer (around  display-buffer-return-to-sender activate)
+  "displaying a buffer in another frame should not change the frame we are currently in!"
+  (let ((f (window-frame (selected-window))))
+    ad-do-it
+    (raise-frame f)))
+
+
+;; EDITING
+
+(defun kill-to-beginning-of-line ()
+  (interactive)
+  (save-excursion
+    (let ((p1 (point)))
+      (beginning-of-line)
+      (kill-region (point) p1))))
+
+(defun just-no-space ()
+  (interactive)
+  (just-one-space)
+  (delete-backward-char 1))
+
+
+(defun zap-backwards (arg char)
+  (interactive "p\ncZap backwards to char: ")
+  (zap-to-char (- (prefix-numeric-value arg)) char))
+
+
+;; MOVEMENT
+
+(defvar last-jumped-to-char nil
+  "This is the last character jumped to by jump-to-char")
+ 
+
+(defun forward-jump-to-char (arg char)
+  "jump forward to ARGth previous CHAR.  doesn't prompt when ran multiple times
+in a row"
+  (interactive
+   (if (or (eq last-command 'forward-jump-to-char)
+	   (eq last-command 'backward-jump-to-char))
+       (list (or current-prefix-arg 1)
+	     last-jumped-to-char)
+     (progn
+       (call-interactively '(lambda (arg char)
+			      (interactive "p\ncJump to char: ")
+			      (push-mark) ;save the current position
+			      (list arg char))))))
+  (setq last-jumped-to-char char)
+  (forward-char 1)
+  (search-forward (char-to-string char)
+		  nil t arg)
+  (backward-char 1))
+
+
+
+(defun backward-jump-to-char (arg char)
+  "jump backward to ARGth previous CHAR.  doesn't prompt when ran
+multiple times in a row"
+  (interactive
+   (if (or (eq last-command 'backward-jump-to-char)
+	   (eq last-command 'forward-jump-to-char))
+       (list (or current-prefix-arg 1)
+	     last-jumped-to-char)
+     (progn
+       (call-interactively '(lambda (arg char)
+			      (interactive "p\ncJump to char: ")
+			      (push-mark) ;save the current position
+			      (list arg char))))))
+  (setq last-jumped-to-char char)
+  (search-forward (char-to-string char)
+		  nil t (- arg)))
+    
+
+
+;--- scrolling made slower
+
+(defalias 'scroll-down-slow 'scroll-down)
+(defalias 'scroll-up-slow 'scroll-up)
+
+(defadvice scroll-down-slow (before slowly activate)
+  (interactive "p"))
+
+(defadvice scroll-up-slow (before slowly activate)
+  (interactive "p"))
+
+
+;; MISC
+
+(defun verify-exit (arg)
+  (interactive "cDo you want to leave Emacs?")
+  (if (or (equal arg 121) (equal arg 89))
+      (save-buffers-kill-emacs)))
+
+
+(defun insert-time-stamp (arg)
+  "insert time stamp. with prefix argument, make it a comment "
+  (interactive "P")
+  (let ((comment-start (if arg comment-start nil))
+	(comment-end (if arg comment-end nil)))
+    (insert (or comment-start "")
+	    (current-time-string)
+	    (or comment-end ""))))
+
+(defun big (how-big)
+  (interactive "p")
+  "Use a bigger font.  Provide a positive number indicating how big."
+  (let ((fonts '(("fixed" . nil)
+		 ("7x13" . nil)
+		 ("8x13" . nil)
+		 ("9x15" . nil)
+		 ("10x20" . nil)
+		 )))
+    (set-default-font (car (nth how-big fonts)))
+    (eval (cdr (nth how-big fonts)))))
+
+
+
+(defvar frame-mitosis-y 62)
+(defvar frame-mitosis-x1 18)
+(defvar frame-mitosis-x2 842)
+
+(defun frame-mitosis()
+  (interactive )
+
+  (let ((width 88)
+	(height 60)
+	(geom-y frame-mitosis-y)
+	(geom-x1 frame-mitosis-x1)
+	(geom-x2 frame-mitosis-x2)
+	(font "9x15"))
+
+    (set-frame-height (selected-frame) height)
+    (set-frame-width (selected-frame) width)
+    (set-frame-position (selected-frame) geom-x2 geom-y)
+    (big 3)
+
+    (let ((after-make-frame-functions (cons '(lambda (frame)
+					       (set-frame-height frame height)
+					       (set-frame-width frame width)
+					       (set-frame-position frame geom-x1 geom-y))
+					    after-make-frame-functions))
+	  (default-frame-alist
+	    (cons (cons 'font font)
+		  default-frame-alist)))
+      (let ((f (new-frame)))
+	(select-frame f)
+	(big 3)
+	(color *color-selection*)))))
+
+
+(defun mono-framify()
+  (interactive)
+  (if (> (length (frame-list)) 1)
+      (delete-frame))
+    
+  (let ((f (car (frame-list))))
+    (set-frame-height f 60)
+    (set-frame-width f 88)
+    (set-frame-position f 309 75)))
+
+
+
+; have the diary print out the contents of todo file, if the current date is
+; selected
+
+(defvar todo-file "~/.todo")
+
+(defun diary-display-todo-file ()
+  (save-excursion
+    (set-buffer "*Calendar*")
+    (if (equal (calendar-current-date)
+	       (calendar-cursor-to-date))
+	(progn
+	  (set-buffer (or (get-buffer fancy-diary-buffer)
+			  (make-fancy-diary-buffer)))
+	  (goto-char (point-max))
+	  (if (file-readable-p todo-file)
+	      (progn
+		(toggle-read-only)
+		(message "Inserting todo-file")
+		(insert "\n\nTODO:\n")			  
+		(insert-file todo-file)
+		(toggle-read-only))))
+      t
+)))
+
+
+(defun edit-todo-file ()
+  (interactive)
+  (if (and todo-file
+	   (file-readable-p todo-file))
+      (find-file-other-window todo-file)
+    (error "What TODO file?!")))
+
+
+;; (defun prefix-paragraph ()
+;;   (interactive)
+;;   (let ((fill-prefix (buffer-substring
+;; 		      (save-excursion (move-to-left-margin) (point))
+;; 		      (point))))
+;;     (refill-paragraph)))
+
+
+(defvar tail-display-buffer 'display-buffer)
+(defun tail-file (pre file)
+  (interactive "P\nfTail file:")
+  (let* ((args (if pre
+		   (read-from-minibuffer "Tail switches :" "-f")
+		 "-f"))
+	 (buf (generate-new-buffer
+	       (concat "*tail " file "*"))))
+    (shell-command (concat "tail " args " " file " &") buf)
+    (run-hooks 'tail-file-hook);
+    (funcall tail-display-buffer buf)
+))
+
+
+(defun shrink-other-window-if-larger-than-buffer ()
+  (interactive)
+  (shrink-window-if-larger-than-buffer (next-window)))
+
+(defun interactively-enlarge-window (arg)
+  (interactive "nHow much: ")
+  (enlarge-window arg))
+
+(defun selectively-delete-lines (arg)
+  (interactive "P")
+  (call-interactively (or (and arg
+			       'delete-non-matching-lines)
+			  'delete-matching-lines)))
+
+;; eval-buffer-as-perl-script caveats:
+
+
+;;  one thing to note is that perl is run under bash under emacs.
+;;  If the command line args you specify don't work in the shell, then
+;;  you get a bash error in the perl output buffer..
+
+;;  if the perl script is expecting input from the user, it will just
+;;  hang if you run it. ( but it hangs in the background, so it won't
+;;  slow you down)-- you have to manually kill it (or exit emacs.. but
+;;  why would you want to do that?)
+
+;;  syntax checking works fine however....
+
+
+(defun eval-buffer-as-perl-script (pre)
+  "Send the current buffer to a perl interpreter for syntax checking.  With
+a prefix arg, run it in another window"
+  (interactive "P")
+  
+  (if (not buffer-file-name)
+      (error "No file for current buffer"))
+  (save-some-buffers)
+
+  (let* ((line-args (if pre (read-from-minibuffer "Command-line args: ")
+		      ""))
+	 (perl-buffer-file-name  (concat
+				  "*Perl: "
+				  buffer-file-name))
+	 (buf (and pre (or (get-buffer perl-buffer-file-name)
+			   (generate-new-buffer perl-buffer-file-name)))))
+
+    (shell-command (concat "perl "
+			   (if pre
+			       ""
+			     "-c ")
+			   buffer-file-name
+			   " " line-args " "
+			   (if pre " &" ""))
+		   (if pre buf nil))
+    (if pre
+	(display-buffer buf))))
+
+
+(defun make-perl-script ()
+  (interactive)
+  (executable-set-magic "perl" "-w")
+  (perl-mode))
+
+
+;; hostname mode
+(defvar hostname (let ((str (or (getenv "HOSTNAME") "")))
+  (substring  str 0 (string-match "\\." str))))
+
+;modified from code for column-number-mode
+(defvar host-name-mode nil
+  "*Non-nil means display host name in mode line.")
+
+(defun host-name-mode (arg)
+  "Toggle Host Name mode.
+With arg, turn Host Name  mode on iff arg is positive.
+When Host Name mode is enabled, the column number appears
+in the mode line."
+  (interactive "P")
+  (setq host-name-mode
+	(if (null arg) (not host-name-mode)
+	  (> (prefix-numeric-value arg) 0)))
+  (force-mode-line-update))
+
+
+(defun filter (pred ls)
+  "filter PRED LS   returns new list of elts for which PRED is true"
+  (if ls
+      (if (funcall pred (car ls))
+	  (cons (car ls) (filter pred (cdr ls)))
+	(filter pred (cdr ls)))
+    '()))
+
+;; a better filter?
+;; (defun my-filter (condp lst)
+;;     (delq nil
+;; 	  (mapcar (lambda (x) (and (funcall condp x) x)) lst)))
+
+
+
+
+(defun base-quoted-insert (prefix)
+  "Replacement for quoted-insert.  It defaults to base-16, but uses prefix argument as the radix"
+  (interactive "P")
+  (let* ((numprefix (prefix-numeric-value prefix))
+	(read-quoted-char-radix (if (and prefix (> numprefix 0))
+				    numprefix
+				  16)))
+    (command-execute 'quoted-insert)))
+
+
+(defun dired-hide-dotfiles()
+  "reload current dired buffer, hiding dotfiles"
+  (interactive)
+  (dired-unmark-all-marks)
+  (dired-mark-files-regexp "^\\.")
+  (dired-do-kill-lines))
+
+
+
+
+(defvar tmp-code-dir "~/Desktop")
+(defun make-tmp-code (fname)
+  "create file arg in a random  directory"
+  (interactive "sfilename: ")
+  (let* ((str (file-name-sans-extension fname))
+	 (dir (concat tmp-code-dir "/" str))
+	 (file (concat dir "/" fname)))
+    (mkdir dir)
+    (find-file file)))
+
---
+++ elisp/site-lisp/ctanis/hooks.el
@@ -0,0 +1,205 @@
+;; auto-load
+(setq auto-mode-alist (cons '("\\.h$" . c++-mode) auto-mode-alist))
+(autoload 'uncompress-while-visiting "uncompress")
+(autoload 'tar-mode "tar-mode" "View .tar files" nil nil)
+
+(mapcar '(lambda (a)
+	   (add-to-list 'auto-mode-alist a))
+	'(("\\.Z$" . uncompress-while-visiting)
+	  ("\\.gz$" . uncompress-while-visiting)
+	  ("\\.h$" . c-mode)
+	  ("\\.m$" . octave-mode)
+	  ))
+
+
+
+(setq popper-load-hook 
+     '(lambda ()
+       (setq popper-pop-buffers t)  ; Make popper pop everything temporary
+       (setq popper-buffers-to-skip t) 
+       (setq popper-use-message-buffer nil)
+
+       ;; Make Webster windows default to 12 lines
+       (add-to-list 'popper-min-heights (cons "^\\Webster" 12))
+       (add-to-list 'popper-min-heights (cons "^\\*tail" 25))
+
+       ;; Don't skip over *Buffer List*
+       (setq popper-buffers-no-skip (cons "*Buffer List*" 
+					  popper-buffers-no-skip))))
+
+(if (string-match "^19.34" emacs-version)
+    (load "popper-19.34")
+  (load "popper"))
+
+
+;; MODE HOOKS
+
+(add-hook 'c-mode-common-hook
+	  '(lambda ()
+	     ;(setq c-basic-offset 4)
+	     (local-set-key "\C-c\C-c" 'compile)
+	     (local-set-key "\C-m" 'newline-and-indent)))
+
+
+;this is the  c comment-region thing i wrote
+(add-hook 'c-mode-hook
+	  '(lambda ()
+	     (c-set-style "k&r")
+	     (local-set-key "\M-o1" 'make-c-header)
+;	     (define-key c-mode-map "\C-c;" 'c-make-block-comment)
+	     ))
+
+;; do i want this?
+(add-hook 'c++-mode-hook
+	  '(lambda ()
+	     (c-set-style "stroustrup")))
+	     
+
+(add-hook 'java-mode-hook
+	  '(lambda ()
+	     (c-set-style "java")))
+
+
+(add-hook 'objc-mode-hook
+	  '(lambda()
+	     (c-set-style "k&r")))
+
+(add-hook 'f90-mode-hook
+	  '(lambda()
+	     (local-set-key "\C-m" 'newline-and-indent)
+	     (local-set-key "\C-c\C-c" 'compile)
+	     (setq f90-do-indent 2)
+	     (setq f90-if-indent 2)
+	     (setq f90-type-indent 2)
+	     (setq f90-program-indent 2)
+	     ))
+
+
+(add-hook 'text-mode-hook
+	  '(lambda ()
+	     (progn
+	       (define-key text-mode-map "\M-s" 'goto-line)
+	       (set-fill-column 68)
+	       ;(auto-fill-mode 1)
+	       (auto-save-mode 1))))
+
+
+
+(add-hook 'comint-output-filter-functions
+	  'shell-strip-ctrl-m)
+
+
+
+
+
+;calendar stuff
+;(add-hook 'today-visible-calendar-hook 'calendar-mark-today)
+(add-hook 'diary-display-hook 'diary-display-todo-file t)
+(add-hook 'diary-display-hook 'fancy-diary-display)
+(add-hook 'list-diary-entries-hook 'sort-diary-entries t)
+
+
+(add-hook 'asm-mode-set-comment-hook '(lambda ()
+					(setq asm-comment-char '?!)))
+
+;(add-hook 'sh-mode-hook '(lambda ()
+;			   (sh-set-shell "zsh")))
+
+;redefinitions of shell-mode keys
+(add-hook 'comint-mode-hook
+	  '(lambda ()
+	     (setq comint-use-prompt-regexp t)
+	     (setq comint-prompt-regexp "^[^\$]+\$ ")
+	     (define-key comint-mode-map
+	       "\M-p" 'comint-previous-matching-input-from-input)
+	     (define-key comint-mode-map
+	       "\M-n" 'comint-next-matching-input-from-input)
+	     (define-key comint-mode-map "\C-a" 'comint-bol)
+	     (define-key comint-mode-map "\M-o\C-?" 'comint-kill-input)))
+
+
+(add-hook 'octave-mode-hook
+	  '(lambda ()
+	     (local-set-key "\C-m" 'newline-and-indent)))
+
+
+(add-hook 'inferior-octave-mode-hook
+	  '(lambda ()
+	     (local-set-key
+	       "\M-p" 'comint-previous-matching-input-from-input)
+	     (local-set-key
+	       "\M-n" 'comint-next-matching-input-from-input)
+	     (local-set-key
+	      "\C-a" 'comint-bol)))
+
+
+(add-hook 'emacs-lisp-mode-hook
+	  '(lambda ()
+	     (setq comment-start ";; ")
+	     (local-set-key "\C-m" 'newline-and-indent)))
+
+(add-hook 'lisp-interaction-mode-hook
+	  '(lambda ()
+	     (setq comment-start ";; ")
+	     (local-set-key "\C-m" 'newline-and-indent)))
+
+
+(add-hook 'perl-mode-hook
+	  '(lambda ()
+	     (local-set-key "\C-c\C-c" 'eval-buffer-as-perl-script)
+	     (local-set-key "\C-m" 'newline-and-indent)))
+
+
+
+(add-hook 'initial-calendar-window-hook
+	  (lambda ()
+	    (if window-system
+		(progn
+		  (set-face-background 'holiday-face "darkslategray")
+		  (set-face-foreground 'calendar-today-face "yellow")
+		  (set-face-foreground 'diary-face "orange")
+		  (set-face-foreground 'holiday-face "cyan")))))
+
+(add-hook 'Man-mode-hook '(lambda ()
+			    (local-set-key "\M-n" 'scroll-up-slow)
+			    (local-set-key "\M-p" 'scroll-down-slow)))
+
+
+(add-hook 'bookmark-load-hook
+	  '(lambda ()
+	     (popper-wrap 'bookmark-show-annotation
+			  "*Bookmark Annotation*")))
+
+;; (add-hook 'tail-file-hook
+;; 	  '(lambda ()
+;; 	     (popper-wrap 'tail-file "*tail")
+;; 	     (setq tail-display-buffer 'popper-switch)
+;; ))
+
+(add-hook 'sgml-mode-hook
+	  '(lambda ()
+	     (auto-fill-mode nil)))
+
+(add-hook 'dired-mode-hook
+	  '(lambda ()
+	     (set-face-foreground 'dired-marked "lightblue")
+	     (local-set-key "h" 'dired-hide-dotfiles)
+	     (local-set-key "\C-c\C-q" 'wdired-change-to-wdired-mode)))
+
+(add-hook 'octave-mode-hook
+	  '(lambda ()
+	     (local-set-key " " 'self-insert-command)))
+
+(add-hook 'inferior-octave-mode-hook
+	  '(lambda ()
+	     (setq comint-prompt-regexp "^[^>]+> ")))
+
+(autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
+ 
+(autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t) 
+
+
+
+(add-hook 'inferior-python-mode-hook
+	  '(lambda ()
+	     (setq comint-prompt-regexp ">>> ")))
