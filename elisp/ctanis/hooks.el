9d0b9671a6989b290c5a85ad6d4ef7e2e05243d3
Craig Tanis <craig-tanis@utc.edu>
2012-10-08 11:02:10 -0400

    tweaks

--- elisp/ctanis/hooks.el
+++ elisp/ctanis/hooks.el
@@ -12,6 +12,7 @@
 
 (add-hook 'markdown-mode-hook
 	  '(lambda()
+	     (setq tab-width 6)
 	     (local-set-key "\C-\M-u" 'backward-up-list)
 	     (local-set-key "\M-p" 'scroll-down-slow)
 	     (local-set-key "\M-n" 'scroll-up-slow)
