4d3394a3182293925812bedb05aab2fee1a07a1d
Craig Tanis <craig-tanis@utc.edu>
2012-10-08 13:33:17 -0400

    tweaks; added shortcat key

--- scripts/tweak_lion.sh
+++ scripts/tweak_lion.sh
@@ -105,3 +105,6 @@ defaults write com.apple.iTunes disablePingSidebar -bool true
 
 # Disable all the other Ping stuff in iTunes
 defaults write com.apple.iTunes disablePing -bool true
+
+## shortcat activation key
+defaults write com.sproutcube.shortcat activationShortcut "m+SHIFT+OPTION"
