796d1c78ca8009fee151abcef4fe0ab702523198
Craig Tanis <craig-tanis@utc.edu>
2013-05-10 13:46:13 -0400

    org-cycle-global-at-bob t

--- elisp/ctanis/init.el
+++ elisp/ctanis/init.el
@@ -470,6 +470,7 @@ For details of keybindings, see `ido-find-file'."
 (setq org-cycle-separator-lines 2)
 
 (setq org-M-RET-may-split-line '((headline . nil) (item . nil) (table . t)))
+(setq org-cycle-global-at-bob t)
 
 (setq org-export-with-sub-superscripts nil)
 					; don't want to see TOC and postamble in my exported html
