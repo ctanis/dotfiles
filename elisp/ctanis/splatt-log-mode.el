5e90e3cdddbcf9dbff3a06a3f2093f977474ec9c
Craig Tanis <craig-tanis@utc.edu>
2013-05-20 18:20:05 -0400

    nice color fontification for log file; also, sample mode&font-lock spec

--- elisp/ctanis/splatt-log-mode.el
+++ elisp/ctanis/splatt-log-mode.el
@@ -1,14 +1,17 @@
 (make-face 'splatt-log-face)
 (make-face 'splatt-debug-face)
 (make-face 'splatt-error-face)
+(make-face 'splatt-trace-face)
 (defconst splatt-log-face 'splatt-log-face)
 (defconst splatt-error-face 'splatt-error-face)
 (defconst splatt-debug-face 'splatt-debug-face)
+(defconst splatt-trace-face 'splatt-trace-face)
 
 (defconst splatt-log-font-lock-keywords
   '(("^<<.*" . splatt-log-face)
     ("^(.*" . splatt-debug-face)
     ("ERROR.*" . splatt-error-face)
+    ("^T.*" . splatt-trace-face)
     ))
 
 (set-face-background 'splatt-log-face "grey")
@@ -17,7 +20,19 @@
 (set-face-foreground 'splatt-error-face "yellow")
 (set-face-background 'splatt-debug-face "grey90")
 (set-face-foreground 'splatt-debug-face "blue")
+(set-face-background 'splatt-trace-face "yellow")
+(set-face-foreground 'splatt-trace-face "blue")
 
+
+(defun splatt-log-previous-trace ()
+  (interactive)
+  (search-backward-regexp "^T\("))
+
+(defun splatt-log-next-trace ()
+  (interactive)
+  (forward-line)
+  (search-forward-regexp "^T\(")
+  (beginning-of-line))
 
 
 (define-derived-mode splatt-log-mode fundamental-mode
@@ -27,6 +42,8 @@
 			     t
 			     t))
   
+  (local-set-key "\C-\M-p" 'splatt-log-previous-trace)
+  (local-set-key "\C-\M-n" 'splatt-log-next-trace)
   (toggle-truncate-lines 1)
   (auto-revert-mode)
   )
