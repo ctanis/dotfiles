170996dbb2c8c15a0853eb9ca5b6c747b2bf7c4f
Craig Tanis <craig-tanis@utc.edu>
2013-05-26 14:39:58 -0400

    added melpa repository

--- elisp/ctanis/init.el
+++ elisp/ctanis/init.el
@@ -385,6 +385,7 @@ For details of keybindings, see `ido-find-file'."
 
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/" ))
 (package-initialize) ;; trigger elpa packages
 
 ;; org-mode
