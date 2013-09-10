f2b459453438baf33b1dd9daff482fbaa2595968
Craig Tanis <craig-tanis@utc.edu>
2013-09-10 18:42:35 -0400

    now calls plain text finder-undock.applescript

--- bin/nofinder.sh
+++ bin/nofinder.sh
@@ -2,7 +2,7 @@ if [ $1 ]; then
  sleep $1
 fi
 
-osascript ~/.dotfiles/scripts/finder-undock.scpt
+osascript ~/.dotfiles/scripts/finder-undock.applescript
 
 ## assumes that Path Finder has been used to enable the Finder's
 ## remove menu item in the dock
