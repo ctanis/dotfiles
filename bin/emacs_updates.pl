674fcacc94ceee8d1c193a6e330a89351847e9c7
Craig Tanis <craig-tanis@utc.edu>
2013-09-13 14:43:19 -0400

    check for emacs package updates and growl

---
+++ bin/emacs_updates.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+$count_cmd = 'emacs -Q -batch -l ~/.dotfiles/elisp/ctanis/package-load.el -e get-updatable-count 2>/dev/null |sed -e s/\"//g ';
+$growl_cmd = 'growlnotify -t "emacs update" -a Emacs.app -s';
+
+$msg = `$count_cmd`;
+if ($msg > 0) {
+
+  open GROWL, "|$growl_cmd" or die;
+  print GROWL $msg;
+  close GROWL;
+
+}
+else  {
+  print STDERR "no updates\n";
+}
