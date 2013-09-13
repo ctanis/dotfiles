faa4a35acf76d6515dd346253aaf66a4c2a6a629
Craig Tanis <craig-tanis@utc.edu>
2013-09-13 09:43:58 -0400

    commented terminal-notifier gem usage

--- bin/count_emacs_packages.sh
+++ bin/count_emacs_packages.sh
@@ -1,1 +1,2 @@
-emacs -Q -batch -l ~/.dotfiles/elisp/ctanis/package-load.el -e get-updatable-count 2>/dev/null |sed -e s/\"//g
+#emacs -Q -batch -l ~/.dotfiles/elisp/ctanis/package-load.el -e get-updatable-count 2>/dev/null |sed -e s/\"//g |terminal-notifier -title "Emacs Updates" -open "osascript -e 'tell application \"Terminal\" to do script with command \"update_emacs.sh\"'"
+emacs -Q -batch -l ~/.dotfiles/elisp/ctanis/package-load.el -e get-updatable-count 2>/dev/null |sed -e s/\"//g 
