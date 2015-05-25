8d0533e4d539145b3debc45590e8c3563eda4a56
Craig Tanis <ctanis@melvin.local>
2015-05-25 14:13:56 -0400

    no hungry defaults

--- elisp/ctanis/hooks.el
+++ elisp/ctanis/hooks.el
@@ -109,7 +109,7 @@
 	     (local-set-key "\C-c\C-c" 'compile)
 	     (local-set-key "\C-m" 'newline-and-indent)
 	     (c-toggle-auto-newline 1)
-	     (c-toggle-hungry-state 1)
+	     (c-toggle-hungry-state -1)
 	     (abbrev-mode -1)
 	     (cwarn-mode 1)
 					; (local-set-key "}" 'self-insert-command)
