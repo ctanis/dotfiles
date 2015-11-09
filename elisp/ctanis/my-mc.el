72061eff80217349ab1f1d683de2522c553e539c
Craig Tanis <craig-tanis@utc.edu>
2015-11-09 12:13:03 -0500

    better mc/search keys

--- elisp/ctanis/my-mc.el
+++ elisp/ctanis/my-mc.el
@@ -15,18 +15,24 @@
 
 
 (when (require-verbose 'multiple-cursors)
-  ;; (require 'phi-search)
-  ;; (require 'phi-search-dired)
+
   (require 'mc-extras)
+  (require 'phi-search)
+  (require 'phi-replace)
+  (require 'phi-search-mc)
+  ;; (require 'phi-search-dired)
+
+  (global-set-key "\M-%" 'phi-replace-query)
 
   ;; (global-set-key (kbd "C-s") 'phi-search)
   ;; (global-set-key (kbd "C-r") 'phi-search-backward)
 
-  ; (define-key dired-mode-map (kbd "/") 'phi-search-dired)
+                                        ; (define-key dired-mode-map (kbd "/") 'phi-search-dired)
   
+  (define-key craig-prefix-map "\M-." (make-sparse-keymap))
   (define-key craig-prefix-map "\M-.\M-l" 'mc/edit-lines)
   (define-key craig-prefix-map "\M-.\M-." 'mc/mark-next-like-this)
-  (define-key craig-prefix-map "\M-.*" 'mc/mark-all-dwim)
+  (define-key craig-prefix-map "\M-.!" 'mc/mark-all-dwim)
   (define-key craig-prefix-map "\M-.@" 'mc/mark-more-like-this-extended)
   (define-key craig-prefix-map "\M-.<" 'mc/mark-sgml-tag-pair)
 
@@ -39,8 +45,29 @@
   (define-key mc/keymap (kbd "C-. C-r") 'mc/reverse-regions)
   (define-key mc/keymap (kbd "C-. C-s") 'mc/sort-regions)
 
+  (defvar phi-search-from-isearch-mc/ctl-map
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd "M-.")   'phi-search-from-isearch-mc/mark-next)
+      (define-key map (kbd "M-,")   'phi-search-from-isearch-mc/mark-previous)
+      (define-key map (kbd "M-!") 'phi-search-from-isearch-mc/mark-all)
+      map))
+
+
+
+  (defadvice phi-search-from-isearch-mc/setup-keys
+      (after for-terminal activate)
+    (define-key isearch-mode-map (kbd "M-.") phi-search-from-isearch-mc/ctl-map))
+  
+
+  (define-key phi-search-default-map (kbd "M-.") 'phi-search-mc/mark-next)
+  (define-key phi-search-default-map (kbd "M-,") 'phi-search-mc/mark-previous)
+  (define-key phi-search-default-map (kbd "M-!") 'phi-search-mc/mark-all)
 
   ; (phi-search-mc/setup-keys)
+  (add-hook 'isearch-mode-hook 'phi-search-from-isearch-mc/setup-keys)
+
+
+                                        ; (phi-search-mc/setup-keys)
 
   (if (>= emacs-major-version 24)
       (progn
