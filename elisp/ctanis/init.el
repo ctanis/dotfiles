551bd521d615608a0191593c533f8b870564b4b5
Craig Tanis <craig-tanis@utc.edu>
2016-01-26 14:15:07 -0500

    potentially weird space-related changes

--- elisp/ctanis/init.el
+++ elisp/ctanis/init.el
@@ -55,7 +55,7 @@
 (define-prefix-command 'craig-prefix 'craig-prefix-map)
 
 (define-key global-map "\M-o" 'craig-prefix)
-(define-key craig-prefix-map " " 'just-no-space)
+;(define-key craig-prefix-map " " 'just-no-space)
 (define-key craig-prefix-map "2" 'create-file-mode)
 (define-key craig-prefix-map "3" 'executable-set-magic)
 (define-key craig-prefix-map "4" 'make-perl-script)
@@ -131,6 +131,7 @@
 
 (global-set-key "\C-l" 'recenter)
 
+(global-set-key (kbd "M-S-SPC") 'cycle-spacing)
 
 ;; UNSET
 (global-unset-key "\C-z") ;; no more suspend/iconify
@@ -554,8 +555,10 @@ For details of keybindings, see `ido-find-file'."
 
 
 (when (require-verbose 'ace-jump-mode)
-  (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-  (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+  (define-key craig-prefix-map " " 'ace-jump-mode)
+  ;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+  ;(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+  (setq ace-jump-mode-move-keys (loop for i from ?a to ?z collect i))
   (set-face-foreground 'ace-jump-face-foreground "black")
   (set-face-background 'ace-jump-face-foreground "wheat")
   (setq ace-jump-mode-submode-list
