e7dd10b59ac2bd04a24ff4fae573e6b31d34b0ed
Craig Tanis <craig-tanis@utc.edu>
2016-05-04 13:03:23 -0400

    fixed ace-jump key

--- elisp/ctanis/init.el
+++ elisp/ctanis/init.el
@@ -572,8 +572,8 @@ For details of keybindings, see `ido-find-file'."
 
 
 (when (require-verbose 'ace-jump-mode)
-  (define-key craig-prefix-map "j" 'ace-jump-mode)
-  ;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+  ;(define-key craig-prefix-map "j" 'ace-jump-mode)
+  (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
   ;(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
   (setq ace-jump-mode-move-keys (loop for i from ?a to ?z collect i))
   (set-face-foreground 'ace-jump-face-foreground "black")
