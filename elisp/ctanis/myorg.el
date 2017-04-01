523fabd0dbe7c550309f388d35bacc675b5d61e7
Craig Tanis <craig-tanis@utc.edu>
2017-04-01 13:15:34 -0400

    fixed ACM/IEEE latex export

--- elisp/ctanis/myorg.el
+++ elisp/ctanis/myorg.el
@@ -241,8 +241,22 @@ most localized thing"
      (add-to-list 'org-latex-classes
                   '("IEEEtran"
                     "\\documentclass{IEEEtran}"
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+                  )
+     (add-to-list 'org-latex-classes
+                  '("acmart"
+                    "\\documentclass{acmart}"
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
 
-                    ))
+                    )
      ))
 
 
