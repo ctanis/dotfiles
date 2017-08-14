db674ef5c7fb19b166a239ef8769ed5ca06199af
Craig Tanis <craig-tanis@utc.edu>
2017-08-14 15:36:38 -0400

    packages include melpa (unstable)

--- elisp/ctanis/package-load.el
+++ elisp/ctanis/package-load.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/" ))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/" ))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/" ) t)
 
 (package-initialize) ;; trigger elpa packages
