7dac52b851169648fe301450064dc3f2d7a8adc1
Craig Tanis <crt@weblar.org>
2019-06-25 11:33:19 -0400

    deft git experiments

--- elisp/ctanis/init.el
+++ elisp/ctanis/init.el
@@ -531,39 +531,7 @@ For details of keybindings, see `ido-find-file'."
 
 ;; deft
 (when (require-verbose 'deft)
-  (setq deft-auto-save-interval 0)
-  (setq deft-text-mode 'org-mode)
-  (setq deft-default-extension "txt")
-  (setq deft-use-filename-as-title t)
-  (setq deft-use-filter-string-for-filename t)
-  (setq deft-file-naming-rules '((noslash . "-")
-                                 (nospace . "_")
-                                 (case-fn . downcase)))
-
-  (define-key craig-prefix-map "\M-0" 'deft)
-
-  (add-hook 'deft-mode-hook
-            (lambda ()
-              (hl-line-mode 1)))
-
-
-  ;; support multiple deft directories
-  (defun rotate-deft-dirs ()
-    (interactive)
-    (let ((mode (car deft-dirs)))
-      (setq deft-directory (car mode))
-      (setq deft-default-extension (cadr mode)))
-    (setq deft-dirs (append (cdr deft-dirs) (list (car deft-dirs))))
-    (if (called-interactively-p)
-        (let ((deft-filter-regexp "."))
-          (deft-filter-clear))
-      ))
-  (define-key deft-mode-map (kbd "C-c [") 'rotate-deft-dirs)
-
-  (defvar deft-dirs '(("~/Dropbox/notes" "txt") ("~/Dropbox/work_notes" "org")))
-  ;(rotate-deft-dirs)
-  )
-
+  (load-library "mydeft"))
 
 ;; browse-kill-ring
 (when (require-verbose 'browse-kill-ring)
---
+++ elisp/ctanis/mydeft.el
@@ -0,0 +1,72 @@
+;; i love deft mode
+
+(setq deft-auto-save-interval 0)
+(setq deft-text-mode 'org-mode)
+(setq deft-default-extension "txt")
+(setq deft-use-filename-as-title t)
+(setq deft-use-filter-string-for-filename t)
+(setq deft-file-naming-rules '((noslash . "-")
+                               (nospace . "_")
+                               (case-fn . downcase)))
+
+(define-key craig-prefix-map "\M-0" 'deft)
+
+(defvar deft-dirs '(("~/Dropbox/notes" "txt") ("~/Dropbox/work_notes" "org")))
+                                        ;(rotate-deft-dirs)
+
+
+(defvar deft_git_folders nil)
+
+;; support multiple deft directories
+(defun rotate-deft-dirs ()
+  (interactive)
+  (let ((mode (car deft-dirs)))
+    (setq deft-directory (car mode))
+    (setq deft-default-extension (cadr mode)))
+  (setq deft-dirs (append (cdr deft-dirs) (list (car deft-dirs))))
+  (if (called-interactively-p)
+      (let ((deft-filter-regexp "."))
+        (deft-filter-clear))
+    ))
+(define-key deft-mode-map (kbd "C-c [") 'rotate-deft-dirs)
+
+
+;; experimental deft/git integration
+
+
+(defun deft_pull_all_git()
+  (interactive)
+  (dolist (path deft_git_folders)
+        (let ((default-directory path))
+          (shell-command "git pull")
+          (get-buffer "*Messages*")
+          (get-buffer "*Messages*")))
+  
+  )
+
+(defun deft_push_all_git()
+  (interactive)
+  (dolist (path deft_git_folders)
+        (let ((default-directory path))
+          (shell-command
+           "git add * && git commit --allow-empty-message -m '' && git push -u origin"
+           (get-buffer "*Messages*")
+           (get-buffer "*Messages*"))))
+  )
+
+
+(defun deft_quit_window()
+  (interactive)
+  (deft_push_all_git)
+  (quit-window))
+
+
+(define-key deft-mode-map (kbd "C-c C-q") 'deft_quit_window)
+
+(add-hook 'deft-mode-hook 'deft_pull_all_git)
+
+
+
+;; (add-hook 'deft-mode-hook
+;;           (lambda ()
+;;             (hl-line-mode 1)))
