d6b7b94b096efeaa9ed1101e11dadc964385e9e3
Craig Tanis <craig-tanis@utc.edu>
2019-09-03 09:40:14 -0400

    python completion fix?

--- elisp/ctanis/init.el
+++ elisp/ctanis/init.el
@@ -794,6 +794,8 @@ For details of keybindings, see `ido-find-file'."
 
 
 (setq python-shell-interpreter "python3")
+(setq python-shell-completion-native-enable nil)
+
 (setq enable-remote-dir-locals t)
 
 (when (require-verbose 'operate-on-number)
