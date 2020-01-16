050d82ed50896f37a78b2e52075d18daa22d73f0
Craig Tanis <craig-tanis@utc.edu>
2020-01-16 10:51:44 -0500

    cleaned up bin

--- bin/nofinder.sh
+++
@@ -1,8 +0,0 @@
-if [ $1 ]; then
- sleep $1
-fi
-
-osascript ~/.dotfiles/scripts/finder-undock.applescript
-
-## assumes that Path Finder has been used to enable the Finder's
-## remove menu item in the dock
