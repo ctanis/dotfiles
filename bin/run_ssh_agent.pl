bcccd3008c1dcb67a6107344cc4e6ec7cfbadb94
Craig Tanis <crt@weblar.org>
2020-12-03 14:02:22 -0500

    perl script for manually managing ssh-agent

---
+++ bin/run_ssh_agent.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+use strict;
+  
+my $cache_file="$ENV{HOME}/.ssh-agent";
+
+my $running_pid=-1;
+my $cached_pid=-1;
+my @cache;
+my $resave_cache=0;
+
+## check for running agnet
+{
+  my @pid = `ps aux |gawk '/ssh-agent/ { print \$1 }'`;
+  chomp @pid;
+  
+  die "too many ssh-agent processes: @pid\n" if scalar @pid > 1;
+  $running_pid=$pid[0] if scalar @pid == 1;
+}
+
+if (-e $cache_file) {
+
+  open CACHE, $cache_file or die "cannot read $cache_file: $!";
+  @cache=<CACHE>;
+  ($cached_pid) = $cache[2]=~m/echo Agent pid (\d+)/;
+  close CACHE;
+}
+
+
+if ($running_pid == -1) {
+  @cache=`ssh-agent -s`;
+  $resave_cache=1;
+} else {
+  die "ssh-agent cache out of sync with reality\n" if $cached_pid ne $running_pid;
+}
+
+print "@cache\n";
+
+if ($resave_cache) {
+  open CACHE, ">$cache_file" or die "cannot open $cache_file: $!";
+
+  print CACHE @cache;
+  close CACHE;  
+}
