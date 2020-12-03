a42653835b80ede81ca5ed3be65fbe141547b8a8
Craig Tanis <crt@weblar.org>
2020-12-03 14:44:43 -0500

    silenced echo line

--- bin/run_ssh_agent.pl
+++ bin/run_ssh_agent.pl
@@ -21,13 +21,14 @@ if (-e $cache_file) {
 
   open CACHE, $cache_file or die "cannot read $cache_file: $!";
   @cache=<CACHE>;
-  ($cached_pid) = $cache[2]=~m/echo Agent pid (\d+)/;
+  ($cached_pid) = $cache[2]=~m/#echo Agent pid (\d+)/;
   close CACHE;
 }
 
 
 if ($running_pid == -1) {
   @cache=`ssh-agent -s`;
+  $cache[2]="#".$cache[2];
   $resave_cache=1;
 } else {
   die "ssh-agent cache out of sync with reality\n" if $cached_pid ne $running_pid;
