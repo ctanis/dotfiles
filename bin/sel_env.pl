5dcfdaf1e83a1051372f15fcfb46f2bdcad98087
Craig Tanis <craig.tanis@hexagon.com>
2021-02-10 09:01:17 -0800

    cool script for selecting env from conf file

---
+++ bin/sel_env.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl -w
+use strict;
+
+my ($tag, $path) = @ARGV;
+my $export_mode='bash';
+
+#print "getting $tag from $path\n";
+if ($tag eq '-') {
+  open DAT, "$path" or die "cannot open $path";
+  ## option listing mode
+  while (<DAT>) {
+    my $new_tag;
+
+    if (($new_tag) = m/^\s*\[([a-z0-9_]+)\]/i) {
+      print "$new_tag " 
+    }
+
+  }
+  print "\n";
+  exit;
+}
+
+my %newenv;
+## turn off all variables in this file
+open DAT, "$path" or die "cannot open $path";
+while (<DAT>) {
+  my ($expvar) = m/^\s*([a-z0-9_]+)\s*=/i;
+  next unless $expvar;
+  $newenv{$expvar}=undef;
+}
+
+
+
+
+## select block
+open DAT, "$path" or die "cannot open $path";
+my $in_block=1;
+my %vars;
+
+while (<DAT>) {
+  chomp;
+  my ($new_tag, $varname, $varvalue);
+  
+  next if m/^\s*#/;
+  next if m/^\s*$/;
+
+  # selection
+  if (($new_tag) = m/^\s*\[([a-z0-9_]+)\]/i) {
+    if (lc $new_tag eq lc $tag) {
+      $in_block = 1;
+    } else {
+      $in_block = 0;
+    }
+    next;
+  }
+
+  if ($in_block) {
+    s/%([a-z_]+)%(?!=)/$vars{$1}/gi;
+
+    if (($varname,$varvalue)= m/^\s*%([a-z0-9_]+)%=(.*)\s*$/i) {
+      $vars{$varname}=$varvalue;
+    }
+    else {
+      my ($k,$v) = m/^\s*([a-z0-9_]+)\s*=\s*(.*)$/i;
+      $newenv{$k}=$v;
+    }
+  }
+}
+
+if ($export_mode eq 'bash') {
+  for my $k (keys %newenv) {
+    if (defined $newenv{$k}) {
+      print "export $k=$newenv{$k};\n";
+    }
+    else {
+      print "unset $k;\n";
+    }
+  }
+
+} else {
+  die "unsupported mode: $export_mode\n";
+}
+      
