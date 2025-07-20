d0ce8ee99effcfee5036808dcdca3c62850485e4
Craig Tanis <crt@weblar.org>
2025-07-19 23:58:01 -0700

    embark keybinding tweaks #2

--- dotemacs/my-vert.el
+++ dotemacs/my-vert.el
@@ -33,3 +33,18 @@
 (define-key vertico-map (kbd "C-j") #'vertico-exit-input)
 (define-key embark-become-file+buffer-map (kbd "r")
             #'consult-recent-file)
+
+;; i want "become" to be "b" not "B"
+(mapcar (lambda (p) (let ((sym (cadr p)))
+                      (if (and (boundp sym) (keymapp (symbol-value sym)))
+                          (define-key (symbol-value sym)
+                                      "b" #'embark-become))))
+        embark-keymap-alist)
+(define-key embark-file-map "B" #'byte-recompile-file)
+
+(defun crt/bounce-to-recent (s)
+  (interactive "F")
+  (call-interactively #'consult-recent-file))
+(define-key embark-file-map "r" #'crt/bounce-to-recent)
+(define-key embark-file-map "R" #'rename-file)
+(define-key embark-file-map "g" #'magit-status)
